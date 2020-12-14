04e9b831df76c173e0ad617be47c73e1d877bff4
KANBE8810 <s19C1034BQ@s.chibakoudai.jp>
2020-12-14 13:02:21 +0900

    fix

--- crane_x7_examples/scripts/OpenCV/opencv.py
+++ crane_x7_examples/scripts/OpenCV/opencv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #-*- coding:utf-8 -*-
 import sys
 import rospy
@@ -6,25 +7,24 @@ import numpy as np
 from std_msgs.msg import String
 from std_msgs.msg import Int32
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeErrofrom cv_bridge import CvBridge
+from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge, CvBridgeError
 
-class coverter:
+class image_converter:
     def __init__(self):
         self.image_pub = rospy.Publisher("image_topic", Image, queue_size=1)
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback)
 
         # 赤色の検出
-    def detect_red_color(self, cv_img):
-        pub = rospy.Publisher("bottle_size", Int32, queue_size=1)
+    def callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        h,w,c = img.shape
         # HSV色空間に変換
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
         # 赤色のHSVの値域1
         hsv_min = np.array([0,64,0])
@@ -42,32 +42,43 @@ class coverter:
         contours,hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         areas = np.array(list(map(cv2.contourArea,contours)))
 
-        if len(areas) == 0 or np.max(areas) / (h*w) < 0.005:
-            # 見つからなかったらNoneを返す
-            print("the area is too small")
-            return None
-        else:
-            # 面積が最大の塊の重心を計算し返す
-            max_idx = np.argmax(areas)
-            max_area = areas[max_idx]
-            result = cv2.moments(contours[max_idx])
-            x = int(result["m10"]/result["m00"])
-            y = int(result["m01"]/result["m00"])
-            return (x,y)
+        for i in range(0, len(contours)):
+            if len(contours[i]) > 0:
 
+              # remove small objects
+                if cv2.contourArea(contours[i]) < 500:
+                    continue
+
+                rect = contours[i]
+                x, y, w, h = cv2.boundingRect(rect)
+                if len(areas) == 0 or np.max(areas) / (h*w) < 0.005:
+                    # 見つからなかったらNoneを返す
+                    print("the area is too small")
+                    return None
+                else:
+                # 面積が最大の塊の重心を計算し返す
+                    max_idx = np.argmax(areas)
+                    max_area = areas[max_idx]
+                    result = cv2.moments(contours[max_idx])
+                    x = int(result["m10"]/result["m00"])
+                    y = int(result["m01"]/result["m00"])
+                    pub.publish(x,y)
+        #ウインドウのサイズを変更                                                               
+        cv_half_image = cv2.resize(cv_image,   (0,0),fx=0.5, fy=0.5)
+
+      # ウインドウ表示                                                                         
+        cv2.imshow("Origin Image", cv_image)
+        cv2.waitKey(3)
+
+def main(args):
+    ic = image_converter()
+    rospy.init_node('image_converter', anonymous=True)
+    try:
+      rospy.spin()
+    except KeyboardInterrupt:
+      print("Shutting down")
+    cv2.destroyAllWindows()
 """
-# 入力画像の読み込み
-    img = cv2.imread("test.png")
-
-# 色検出
-    pos= detect_red_color(img)
-    if pos is not None:
-        cv2.circle(img,pos,10,(0,255,0),-1)
-
-# 結果を出力
-    cv2.imwrite("result",img)
-"""
-
 def main():
 # webカメラを扱うオブジェクトを取得
     cap = cv2.VideoCapture(0)
@@ -85,9 +96,9 @@ def main():
         if pos == o_pos:
             flag = False
         o_pos = pos
-
+"""
 if __name__ == '__main__':
-    try:
-        main()
-    except rospy.ROSInterruptException:
-        pass
+    sub = rospy.Subscriber("/red_color", Image, main)
+    main(sys.argv)
+    pub = rospy.Publisher("red_rocate", Int32, queue_size=1)
+    rospy.spin()
